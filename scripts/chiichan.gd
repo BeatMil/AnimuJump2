1e1b134e4133dfd8b95cb997da4ed5f41518903e
beatmil <anutorn@gmail.com>
2023-03-13 02:51:19 +0700

    Add sword_attack01_b

--- scripts/chiichan.gd
+++ scripts/chiichan.gd
@@ -8,10 +8,13 @@ func _ready():
 
 
 func _input(event):
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("attack01"):
 		anim_player.play("attack01")
+
+	if event.is_action_pressed("attack02"):
+		anim_player.play("attack01_b")
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == "attack01":
+	if anim_name == "attack01" or anim_name == "attack01_b":
 		anim_player.play("hopping", -1 , 0.8)
